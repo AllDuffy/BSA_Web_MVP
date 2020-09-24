5198d3880ceb98ead11eb99d33099cf46be79f8b
neerajs6 <neeraj_sudhakar@yahoo.com>
2020-09-24 13:15:05 -0400

    added urls

--- crm1/settings.py
+++ crm1/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'cz=&%f*9(d*zo$_55p=(p)(eki#p$pb^0159-)8k^6$9c3l&_b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bsa-web.herokuapp.com', '127.0.0.1', 'www.bytesizearxiv.org', 'bytesizearxiv.org', ]
+ALLOWED_HOSTS = ['https://bytesizearxiv.org','http://bytesizearxiv.org', 'bsa-web.herokuapp.com', '127.0.0.1', 'www.bytesizearxiv.org', 'bytesizearxiv.org']
 
 
 # Application definition
